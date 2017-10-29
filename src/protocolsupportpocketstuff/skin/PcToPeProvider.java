62f028a91dc4381ddad63186075b114cd07aa368
Leonardo Malaman <leonardomalaman@gmail.com>
2017-10-29 21:52:01 -0200

    Fix PE -> PC skins
    I have no idea what I did, but now it works, so better commit it NOW instead of breaking it by doing unnecessary changes

--- src/protocolsupportpocketstuff/skin/PcToPeProvider.java
+++ src/protocolsupportpocketstuff/skin/PcToPeProvider.java
@@ -1,5 +1,6 @@
 package protocolsupportpocketstuff.skin;
 
+import protocolsupport.api.unsafe.peskins.DefaultPESkinsProvider;
 import protocolsupport.api.unsafe.peskins.PESkinsProvider;
 import protocolsupportpocketstuff.ProtocolSupportPocketStuff;
 import protocolsupportpocketstuff.storage.Skins;
@@ -13,23 +14,23 @@ public class PcToPeProvider extends PESkinsProvider {
 
 	ProtocolSupportPocketStuff plugin;
 	Skins skinCache = Skins.INSTANCE;
-	
+
 	public PcToPeProvider(ProtocolSupportPocketStuff plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@Override
 	public byte[] getSkinData(String url) {
 		if (skinCache.hasPeSkin(url)) {
 			return skinCache.getPeSkin(url);
 		}
-		return null;
+		return DefaultPESkinsProvider.DEFAULT_STEVE;
 	}
 
 	@Override
 	public void scheduleGetSkinData(String url, Consumer<byte[]> skindataApplyCallback) {
 		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
-			
+
 			@Override
 			public void run() {
 				try {
@@ -42,9 +43,9 @@ public class PcToPeProvider extends PESkinsProvider {
 					}
 				} catch (IOException e) { }
 			}
-			
+
 		});
-		
+
 	}
 
 }
